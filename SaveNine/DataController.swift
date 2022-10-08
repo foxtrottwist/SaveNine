2ccc94dbe59a3229d3442951d620073c9cb81af5
Lawrence Horne <lawrencehorne@Citrus-maxima.local>
2022-10-08 19:23:39 -0400

    Added count function to DataController; updated createSampleData function.

--- SaveNine/DataController.swift
+++ SaveNine/DataController.swift
@@ -37,14 +37,45 @@ class DataController: ObservableObject {
         return dataController
     }()
     
+    func count<T>(for fetchRequest: NSFetchRequest<T>) -> Int {
+        (try? container.viewContext.count(for: fetchRequest)) ?? 0
+    }
+    
     func createSampleData() throws {
         let viewContext = container.viewContext
+        let projects = ["OMG David!", "Chip, the Goldfinch", "Deep Dive", "Ursula Minor", "Big G, the Giaffe"]
+        let checklists = ["Supplies", "Todos", "Ideas"]
+        let tags = ["fpp", "quilting", "animals"]
         
-        for p in 1...5 {
+        for p in projects {
             let project = Project(context: viewContext)
-            project.name = "Project \(p)"
+            project.name = p
+            project.id = UUID()
             project.creationDate = Date()
             project.closed = Bool.random()
+            
+            for c in checklists {
+                let checklist = Checklist(context: viewContext)
+                checklist.name = c
+                checklist.items = []
+                checklist.creationDate = Date()
+                checklist.project = project
+                
+                for i in 1...3 {
+                     let item = Item(context: viewContext)
+                     item.name = "Item number \(i)"
+                     item.creationDate = Date()
+                     item.completed = Bool.random()
+                     item.checklist = checklist
+                     item.priority = Int16.random(in: 1...3)
+                 }
+            }
+        }
+        
+        for t in tags {
+            let tag = Ptag(context: viewContext)
+            tag.name = t
+            tag.id = UUID()
         }
         
         try viewContext.save()
@@ -61,12 +92,24 @@ class DataController: ObservableObject {
     }
     
     func deleteAll() {
-        let fetchRequest1: NSFetchRequest<NSFetchRequestResult> = Session.fetchRequest()
+        let fetchRequest1: NSFetchRequest<NSFetchRequestResult> = Item.fetchRequest()
         let batchDeleteRequest1 = NSBatchDeleteRequest(fetchRequest: fetchRequest1)
         _ = try? container.viewContext.execute(batchDeleteRequest1)
         
-        let fetchRequest2: NSFetchRequest<NSFetchRequestResult> = Project.fetchRequest()
+        let fetchRequest2: NSFetchRequest<NSFetchRequestResult> = Checklist.fetchRequest()
         let batchDeleteRequest2 = NSBatchDeleteRequest(fetchRequest: fetchRequest2)
         _ = try? container.viewContext.execute(batchDeleteRequest2)
+        
+        let fetchRequest3: NSFetchRequest<NSFetchRequestResult> = Session.fetchRequest()
+        let batchDeleteRequest3 = NSBatchDeleteRequest(fetchRequest: fetchRequest3)
+        _ = try? container.viewContext.execute(batchDeleteRequest3)
+        
+        let fetchRequest4: NSFetchRequest<NSFetchRequestResult> = Ptag.fetchRequest()
+        let batchDeleteRequest4 = NSBatchDeleteRequest(fetchRequest: fetchRequest4)
+        _ = try? container.viewContext.execute(batchDeleteRequest4)
+        
+        let fetchRequest5: NSFetchRequest<NSFetchRequestResult> = Project.fetchRequest()
+        let batchDeleteRequest5 = NSBatchDeleteRequest(fetchRequest: fetchRequest5)
+        _ = try? container.viewContext.execute(batchDeleteRequest5)
     }
 }
