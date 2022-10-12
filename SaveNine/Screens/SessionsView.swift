870d7e6527cab0696142bb712102c731654b1229
Lawrence Horne <lawrencehorne@Citrus-maxima.local>
2022-10-12 00:04:34 -0400

    Initial sessions export functionality.

--- SaveNine/Screens/SessionsView.swift
+++ SaveNine/Screens/SessionsView.swift
@@ -12,26 +12,71 @@ struct SessionsView: View {
     
     @EnvironmentObject var dataController: DataController
     
+    @State private var document: TextFile?
+    @State var showingSessionsExport = false
+    
     var body: some View {
-        List {
             if sessions.isEmpty || sessions.first?.endDate == nil {
-                Text("No time tracking sessions have been completed for this project.")
-                    .italic()
-                    .foregroundColor(.secondary)
+                VStack {
+                    Text("No time tracking sessions have been completed for this project.")
+                        .italic()
+                        .foregroundColor(.secondary)
+                        .multilineTextAlignment(.center)
+                }
+                .padding()
             } else {
-                ForEach(sessions) { session in
-                    if let duration = session.duration, let startDate = session.startDate, session.endDate != nil {
-                        VStack(alignment: .leading) {
-                            Text(startDate.formatted(date: .abbreviated, time: .shortened))
-                                .font(.callout)
-                                .foregroundColor(.secondary)
-                            Text(format(duration: duration, in: .hours) + format(duration: duration, in: .minutes) + format(duration: duration, in: .seconds))
+                List {
+                    ForEach(sessions) { session in
+                        if let duration = session.duration, let startDate = session.startDate, session.endDate != nil {
+                            VStack(alignment: .leading) {
+                                Text(startDate.formatted(date: .abbreviated, time: .shortened))
+                                    .font(.callout)
+                                    .foregroundColor(.secondary)
+                                Text(formatSession(duration: duration))
+                            }
                         }
                     }
+                    .onDelete(perform: deleteSession)
                 }
-                .onDelete(perform: deleteSession)
+                .toolbar {
+                    EditButton()
+                    
+                    Button {
+                        showingSessionsExport.toggle()
+                        document = TextFile(initialText: export(sessions: sessions))
+                    } label: {
+                        Image(systemName: "square.and.arrow.up")
+                    }
+                }
+                .fileExporter(isPresented: $showingSessionsExport, document: document, contentType: .plainText) { result in
+                    switch result {
+                    case .success(let success):
+                        print(success)
+                    case .failure(let failure):
+                        print(failure)
+                    }
+                }
             }
+    }
+    
+    func export(sessions: [Session]) -> String {
+        return sessions.map {
+            """
+             \($0.startDate!.formatted(date: .abbreviated, time: .shortened))
+             \(formatSession(duration: $0.duration))
+             
+             """
         }
+        .reduce("") {
+            """
+             \($0)
+             \($1)
+             """
+        }
+    }
+    
+    func formatSession(duration: Double) -> String {
+        return format(duration: duration, in: .hours) + format(duration: duration, in: .minutes) + format(duration: duration, in: .seconds)
     }
     
     func deleteSession(at offsets: IndexSet) {
