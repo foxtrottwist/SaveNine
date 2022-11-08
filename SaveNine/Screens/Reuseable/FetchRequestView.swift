d840a602d5ee2bb83db674f511d432510daa2a84
Lawrence Horne <lawrencehorne@Citrus-maxima.local>
2022-11-08 11:11:42 -0500

    Added FetchRequestView

---
+++ SaveNine/Screens/Reuseable/FetchRequestView.swift
@@ -0,0 +1,30 @@
+//
+//  FetchRequestView.swift
+//  SaveNine
+//
+//  Created by Lawrence Horne on 11/8/22.
+//
+
+import CoreData
+import SwiftUI
+
+struct FetchRequestView<T: NSManagedObject, Content: View>: View {
+    @FetchRequest var fetchedResults: FetchedResults<T>
+    
+    let content: (FetchedResults<T>) -> Content
+    
+    init(_ fetchRequest: NSFetchRequest<T>, @ViewBuilder _ content: @escaping (FetchedResults<T>) -> Content) {
+        _fetchedResults  = FetchRequest(fetchRequest: fetchRequest)
+        self.content = content
+    }
+    
+    var body: some View {
+        self.content(fetchedResults)
+    }
+}
+
+struct FetchRequestView_Previews: PreviewProvider {
+    static var previews: some View {
+        FetchRequestView(Project.fetchProjects(predicate: nil, sortDescriptors: nil), { _ in EmptyView() })
+    }
+}
