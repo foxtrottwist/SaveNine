743278285db74a802272b7f4310ae1de99d4c782
Lawrence Horne <lawrencehorne@Citrus-maxima.local>
2022-12-18 17:06:46 -0500

    Added Widget Extension Target.

---
+++ SaveNineWidget/SaveNineWidget.swift
@@ -0,0 +1,68 @@
+//
+//  SaveNineWidget.swift
+//  SaveNineWidget
+//
+//  Created by Lawrence Horne on 12/18/22.
+//
+
+import WidgetKit
+import SwiftUI
+import Intents
+
+struct Provider: IntentTimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+    }
+
+    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), configuration: configuration)
+        completion(entry)
+    }
+
+    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, configuration: configuration)
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let configuration: ConfigurationIntent
+}
+
+struct SaveNineWidgetEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        Text(entry.date, style: .time)
+    }
+}
+
+struct SaveNineWidget: Widget {
+    let kind: String = "SaveNineWidget"
+
+    var body: some WidgetConfiguration {
+        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
+            SaveNineWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+struct SaveNineWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        SaveNineWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
---
+++ SaveNineWidget/SaveNineWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  SaveNineWidgetBundle.swift
+//  SaveNineWidget
+//
+//  Created by Lawrence Horne on 12/18/22.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SaveNineWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        SaveNineWidget()
+        SaveNineWidgetLiveActivity()
+    }
+}
