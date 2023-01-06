55ce21c492b3c8dc207604c9cb3b5818638d8472
Lawrence Horne <lawrencehorne@Citrus-maxima.local>
2023-01-06 17:14:57 -0500

    Updated FileManager.default.urls -> URL.documentsDirectory

--- SaveNine/Helpers/DocumentHelpers.swift
+++ SaveNine/Helpers/DocumentHelpers.swift
@@ -8,20 +8,13 @@
 import Foundation
 import SwiftUI
 
-/// A helper to provide quick reference to the user's document directory.
-/// - Returns: The URL to the document directory.
-func getDocumentsDirectory() -> URL {
-    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-    return paths.first!
-}
-
 /// Saves an UIImage as a jpeg to the user's document directory.
 /// - Parameters:
 ///   - uiImage: The UIImage to save.
 ///   - name: The name of the image file.
 func save(uiImage: UIImage, named name: String) {
         if let data = uiImage.jpegData(compressionQuality: 0.8) {
-            let path = getDocumentsDirectory().appendingPathComponent(name)
+            let path = URL.documentsDirectory.appendingPathComponent(name)
             try? data.write(to: path)
         }
 }
@@ -30,7 +23,7 @@ func save(uiImage: UIImage, named name: String) {
 /// - Parameter name: The name of the image file.
 /// - Returns: The image retrieved or nil if there was no name found for the name provided.
 func getImage(named name: String) -> UIImage? {
-    let path = getDocumentsDirectory().appendingPathComponent(name).path
+    let path = URL.documentsDirectory.appendingPathComponent(name).path
     
     guard FileManager.default.fileExists(atPath: path) else {
            print("Image does not exist at path: \(path)")
@@ -51,7 +44,7 @@ func getImage(named name: String) -> UIImage? {
 /// - Parameter name: The name of the file to deletes.
 func deleteFile(named name: String) {
     guard !name.isEmpty else { return }
-    let url = getDocumentsDirectory().appendingPathComponent(name)
+    let url = URL.documentsDirectory.appendingPathComponent(name)
        
     guard FileManager.default.fileExists(atPath: url.path) else {
            print("File does not exist at path: \(url)")
