5dbfa4fafd52a8ffb8614d37ef042d38ca79d478
Lawrence Horne <lawrencehorne@Citrus-maxima.local>
2023-02-17 19:14:12 -0500

    Deleted duplicate file named Helper; created FileManager+Documents to house logic.

---
+++ SaveNine/Extensions/FileManager+Documents.swift
@@ -0,0 +1,29 @@
+//
+//  FileManager+Documents.swift
+//  SaveNine
+//
+//  Created by Lawrence Horne on 2/17/23.
+//
+
+import Foundation
+
+extension FileManager {
+    /// Deletes a file in the user's document directory if one is found with the provided name.
+    /// - Parameter name: The name of the file to deletes.
+    static func deleteFile(named name: String) {
+        guard !name.isEmpty else { return }
+        let url = URL.documentsDirectory.appending(path: name)
+           
+        guard FileManager.default.fileExists(atPath: url.path()) else {
+               print("File does not exist at path: \(url)")
+               return
+           }
+           
+           do {
+               try FileManager.default.removeItem(at: url)
+               print("\(name) was deleted.")
+           } catch let error as NSError {
+               print("Could not delete \(name): \(error)")
+           }
+    }
+}
