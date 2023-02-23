0d93818aa95820945e9b7412859532a2f73dc5cf
Lawrence Horne <lawrencehorne@Citrus-maxima.local>
2023-02-23 08:48:49 -0500

    Cleanup

--- SaveNine/Views/Projects/ProjectDetailView.swift
+++ SaveNine/Views/Projects/ProjectDetailView.swift
@@ -57,15 +57,11 @@ struct ProjectDetailView: View {
                     .onChange(of: editing, perform: editTags)
             }
         }
-        .navigationTitle(name)
-        .navigationBarTitleDisplayMode(.inline)
-        .onDisappear(perform: dataController.save)
-        .scrollDismissesKeyboard(.interactively)
-        .toolbar {
-            Button(editing ? "Done" : "Edit", action: editProject)
-            detailsMenu
-        }
-        .confirmationDialog("Are you sure you want to delete this project?", isPresented: $showingDeleteConfirm, titleVisibility: .visible) {
+        .confirmationDialog(
+            "Are you sure you want to delete this project?",
+            isPresented: $showingDeleteConfirm,
+            titleVisibility: .visible
+        ) {
             Button("Delete Project", role: .destructive) {
                 delete(project: project)
             }
@@ -73,7 +69,8 @@ struct ProjectDetailView: View {
         .fileExporter(
             isPresented: $showingFileExporter,
             document: document,
-            contentType: .json, defaultFilename: "\(project.projectName)"
+            contentType: .json,
+            defaultFilename: name
         ) { result in
             switch result {
             case .success(let url):
@@ -82,50 +79,65 @@ struct ProjectDetailView: View {
                 print(error.localizedDescription)
             }
         }
+        .navigationTitle(name)
+        .navigationBarTitleDisplayMode(.inline)
+        .onDisappear(perform: dataController.save)
+        .scrollDismissesKeyboard(.interactively)
+        .toolbar {
+            Button(editing ? "Done" : "Edit", action: editProject)
+            Menu {
+                NavigationLink(
+                    destination: SessionsView(
+                        sessions: project.projectSessions,
+                        sharedSessions: project.projectShareSessions
+                    )
+                ) {
+                    Label("Sessions", systemImage: "clock")
+                }
+                
+                NavigationLink(destination: ChecklistView(project: project)) {
+                    Label("Checklists", systemImage: "checklist")
+                }
+                
+                Divider()
+                Button {
+                    document = ProjectDocument.document(from: project)
+                    showingFileExporter.toggle()
+                } label: {
+                    Label("Export", systemImage: "square.and.arrow.up")
+                }
+                .disabled(project.tracking)
+                
+                Button {
+                    toggleProjectClosed()
+                } label: {
+                    if project.closed {
+                        Label("Reopen project", systemImage: "tray.full")
+                    } else {
+                        Label( "Close project", systemImage: "archivebox")
+                    }
+                }
+                .disabled(project.tracking)
+                
+                Divider()
+                Button {
+                    showingDeleteConfirm.toggle()
+                } label: {
+                    Label("Delete Project", systemImage: "trash")
+                        .foregroundColor(.red)
+                }
+                .disabled(project.tracking)
+            } label: {
+                Label("Menu", systemImage: "ellipsis.circle")
+            }
+            .disabled(editing)
+        }
     }
     
-    var detailsMenu: some View {
-        Menu {
-            NavigationLink(destination: SessionsView(sessions: project.projectSessions, sharedSessions: project.projectShareSessions)) {
-                Label("Sessions", systemImage: "clock")
-            }
-            
-            NavigationLink(destination: ChecklistView(project: project)) {
-                Label("Checklists", systemImage: "checklist")
-            }
-            
-            Divider()
-            
-            Button {
-                document = ProjectDocument.document(from: project)
-                showingFileExporter.toggle()
-            } label: {
-                Label("Export", systemImage: "square.and.arrow.up")
-            }
-            .disabled(project.tracking)
-            
-            Button {
-                toggleProjectClosed()
-            } label: {
-                if project.closed {
-                    Label("Reopen project", systemImage: "tray.full")
-                } else {
-                    Label( "Close project", systemImage: "archivebox")
-                }
-            }
-            .disabled(project.tracking)
-
-            Button {
-                showingDeleteConfirm.toggle()
-            } label: {
-                Label("Delete Project", systemImage: "trash")
-                    .foregroundColor(.red)
-            }
-            .disabled(project.tracking)
-        } label: {
-            Label("Menu", systemImage: "ellipsis.circle")
-        }
-        .disabled(editing)
+    func delete(project: Project) {
+        FileManager.deleteImage(named: project.projectImage)
+        dataController.delete(project)
+        dismiss()
     }
     
     func editProject() {
@@ -137,8 +149,48 @@ struct ProjectDetailView: View {
                 project.name = name
             }
         }
-
         editing.toggle()
+    }
+    
+    func editTags(_ editing: Bool) {
+        guard !editing, project.projectTagsString != tags else { return }
+        
+        let tagNames = prepare(tags: tags)
+        tags = tagNames.joined(separator: " ")
+        update(tags: tagNames, in: project)
+    }
+    
+    /// Accepts a string of words and validates that the entered text can be added as a tags.
+    /// - Parameter tags: A string representing the tags already associated with or to be added to the project.
+    /// - Returns: An array of unique strings representing the tags already associated with or to be added to the project.
+    func prepare(tags: String) -> [String] {
+        var set = Set<String>()
+        // Splits the provided string into separate words verifies
+        // that there is only a single instance of a word by attempting
+        // to insert them into a Set.
+        return tags.components(separatedBy: " ").map { $0.lowercased() }
+            .filter { !$0.isEmpty && set.insert($0).inserted }.sorted { $0 < $1 }
+    }
+    
+    /// Handles associating of tags with a given project.
+    /// - Parameters:
+    ///   - tags: An array of strings representing tags to be associated with a project.
+    ///   - project: The Project that will be associated with the provided tags.
+    func update(tags: [String], in project: Project) {
+        let updatedTags = tags.map { tagName in
+            // If a tag with the provided name already exists, return
+            // it instead of creating a new one.
+            if let existingTag = ptags.first(where: { $0.name == tagName }) {
+                return existingTag
+            } else {
+                let newTag = Ptag(context: managedObjectContext)
+                newTag.id = UUID()
+                newTag.name = tagName
+                
+                return newTag
+            }
+        }
+        project.tags = Set(updatedTags) as NSSet
     }
     
     func toggleProjectClosed() {
@@ -160,56 +212,6 @@ struct ProjectDetailView: View {
             FileManager.deleteImage(named: project.projectImage)
             project.image = nil
         }
-    }
-    
-    func editTags(_ editing: Bool) {
-        guard !editing, project.projectTagsString != tags else { return }
-        
-        let tagNames = prepare(tags: tags)
-        tags = tagNames.joined(separator: " ")
-        
-        update(tags: tagNames, in: project)
-    }
-    
-    /// Accepts a string of words and validates that the entered text can be added as a tags.
-    /// - Parameter tags: A string representing the tags already associated with or to be added to the project.
-    /// - Returns: An array of unique strings representing the tags already associated with or to be added to the project.
-    func prepare(tags: String) -> [String] {
-        var set = Set<String>()
-        // Splits the provided string into separate words verifies
-        // that there is only a single instance of a word by attempting
-        // to insert them into a Set.
-        return tags.components(separatedBy: " ").map { $0.lowercased() }
-            .filter { !$0.isEmpty && set.insert($0).inserted }.sorted { $0 < $1 }
-    }
-    
-    
-    /// Handles associating of tags with a given project.
-    /// - Parameters:
-    ///   - tags: An array of strings representing tags to be associated with a project.
-    ///   - project: The Project that will be associated with the provided tags.
-    func update(tags: [String], in project: Project) {
-        let updatedTags = tags.map { tagName in
-            // If a tag with the provided name already exists, return
-            // it instead of creating a new one.
-            if let existingTag = ptags.first(where: { $0.name == tagName }) {
-                return existingTag
-            } else {
-                let newTag = Ptag(context: managedObjectContext)
-                newTag.id = UUID()
-                newTag.name = tagName
-                
-                return newTag
-            }
-        }
-        
-        project.tags = Set(updatedTags) as NSSet
-    }
-    
-    func delete(project: Project) {
-        FileManager.deleteImage(named: project.projectImage)
-        dataController.delete(project)
-        dismiss()
     }
 }
 
