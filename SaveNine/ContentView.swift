826e64aa2bc3602270b2cb3ad9e4ffabc7916420
Law Horne <hello@foxtrottwist.com>
2023-07-30 15:21:25 -0400

    Added PrefersTabNavigationEnvironmentKey to control alternate UI on mac and iPad.

--- SaveNine/ContentView.swift
+++ SaveNine/ContentView.swift
@@ -8,31 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.prefersTabNavigation) private var prefersTabNavigation
     @SceneStorage(StorageKey.selectedTabView.rawValue) private var selectedTabView: String?
     @State private var tabController = TabController()
     
     var body: some View {
-        TabView(selection: $tabController.selectedTabView) {
-            ProjectsSplitView(subject: tabController.subject)
-                .tag(ProjectsSplitView.tag)
-                .tabItem {
-                    Image(systemName: "tray")
-                    Text("Projects")
+        if prefersTabNavigation {
+            TabView(selection: $tabController.selectedTabView) {
+                ProjectsSplitView(subject: tabController.subject)
+                    .tag(ProjectsSplitView.tag)
+                    .tabItem {
+                        Image(systemName: "tray")
+                        Text("Projects")
+                    }
+                
+                
+                SessionsNavigationStack()
+                    .tag(SessionsNavigationStack.tag)
+                    .tabItem {
+                        Image(systemName: "clock")
+                        Text("Sessions")
+                    }
+            }
+            .onAppear(perform: { tabController.selectedTabView = selectedTabView })
+            .onChange(of: tabController.selectedTabView) {  selectedTabView = tabController.selectedTabView }
+            .onOpenURL { _ in
+                if tabController.selectedTabView != ProjectsSplitView.tag {
+                    tabController.selectedTabView = ProjectsSplitView.tag
                 }
-            
-            
-            SessionsNavigationStack()
-                .tag(SessionsNavigationStack.tag)
-                .tabItem {
-                    Image(systemName: "clock")
-                    Text("Sessions")
-                }
-        }
-        .onAppear(perform: { tabController.selectedTabView = selectedTabView })
-        .onChange(of: tabController.selectedTabView) {  selectedTabView = tabController.selectedTabView }
-        .onOpenURL { _ in
-            if tabController.selectedTabView != ProjectsSplitView.tag {
-                tabController.selectedTabView = ProjectsSplitView.tag
             }
         }
     }
