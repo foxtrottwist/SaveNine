ece4c4c02ad70c51f092b4e3de27138be05947e1
Law Horne <hello@foxtrottwist.com>
2023-08-01 18:55:44 -0400

    Updated SessionDetail for SwiftData.

--- SaveNine/Views/Sessions/SessionDetail.swift
+++ SaveNine/Views/Sessions/SessionDetail.swift
@@ -9,10 +9,7 @@ import SwiftUI
 
 struct SessionDetail: View {
     let session: Session
-    
     @Environment(\.dismiss) var dismiss
-    @EnvironmentObject var dataController: DataController
-    
     @State private var duration: Double
     @State private var endDate: Date
     @State private var label: String
@@ -20,7 +17,7 @@ struct SessionDetail: View {
     
     init(session: Session) {
         self.session = session
-        _duration = State(wrappedValue: session.duration)
+        _duration = State(wrappedValue: session.duration!)
         _endDate = State(wrappedValue: session.endDate ?? Date())
         _label = State(wrappedValue: session.sessionLabel)
         _startDate = State(wrappedValue: session.startDate ?? Date())
@@ -68,13 +65,10 @@ struct SessionDetail: View {
     }
     
     private func updateSession() {
-        session.project?.objectWillChange.send()
         session.label = label
         session.startDate = startDate
         session.endDate = endDate
         session.duration = endDate.timeIntervalSince(startDate)
-        
-        dataController.save()
         dismiss()
         
         if let sessionProject = session.project, let project = ProjectWidget.mostRecentlyTrackedProject {
@@ -93,9 +87,6 @@ struct SessionDetail: View {
     }
 }
 
-struct SessionDetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        SessionDetail(session: Session.preview)
-            .environment(SessionLabelController.preview)
-    }
+#Preview {
+    SessionDetail(session: Session.preview)
 }
