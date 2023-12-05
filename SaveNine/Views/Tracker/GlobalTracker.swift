bab8aeefccc744f737c78198791468d350ae128f
Law Horne <hello@foxtrottwist.com>
2023-12-04 22:10:50 -0500

    Saving progress.

--- SaveNine/Views/Tracker/GlobalTracker.swift
+++ SaveNine/Views/Tracker/GlobalTracker.swift
@@ -9,32 +9,30 @@ import SwiftData
 import SwiftUI
 
 struct GlobalTracker: View {
+    @State private var project: Project?
     @State private var offsetY = 100.0
     @Query(filter: Project.predicate(tracking: true)) private var projects: [Project]
     
     var body: some View {
         Color.clear
             .safeAreaInset(edge: .bottom) {
-                if projects.isEmpty {
-                    Tracker(project: projects.first)
-                        .offset(y: offsetY)
-                        .task(id: projects) {
+                Tracker(project: projects.first)
+                    .offset(y: offsetY)
+                    .task(id: projects) {
+                        if projects.isEmpty {
+                            defer { project = nil }
                             withAnimation(.easeInOut.delay(0.5)) {
                                 offsetY = 100.0
                             }
                         }
-                    
-                }
-                
-                if projects.first != nil {
-                    Tracker(project: projects.first)
-                        .offset(y: offsetY)
-                        .task {
-                            withAnimation(.bouncy.delay(0.5)) {
+                        
+                        if projects.first != nil {
+                            defer { project = projects.first }
+                            withAnimation(.bouncy.delay(0.4)) {
                                 offsetY = 0.0
                             }
                         }
-                }
+                    }
             }
     }
 }
